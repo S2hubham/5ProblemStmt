8eec319a82f036aacec58ea715fe70aa3c50c749
S2hubham <143474339+S2hubham@users.noreply.github.com>
2025-05-06 10:41:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    const options = {
+        root: path.join(__dirname)
+    };
+
+    const fileName = './index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.error('Error sending file:', err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
